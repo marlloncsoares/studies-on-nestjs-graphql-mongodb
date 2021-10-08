8e1240e620bc9b1b79948b741ef79702d7c36cf0
marlloncsoares <marlonmcs@hotmail.com>
2021-10-08 19:15:33 -0300

    feat: atualiza cat.resolver

--- src/cat/cat.resolver.ts
+++ src/cat/cat.resolver.ts
@@ -1,7 +1,11 @@
 import { Resolver, Query } from '@nestjs/graphql';
+import { CatService } from './cat.service';
+// import { CatType } from './dto/cat.dto';
 
 @Resolver()
 export class CatResolver {
+  constructor(private readonly catService: CatService) {}
+
   @Query((returns) => String)
   async hello() {
     return 'Olá mundo';
