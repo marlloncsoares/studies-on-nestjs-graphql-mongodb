5792e48078549f639c035bcf45800f8dfde15468
marlloncsoares <marlonmcs@hotmail.com>
2021-10-08 18:13:27 -0300

    feat(cat module): atualiza cat service

--- src/cat/cat.service.ts
+++ src/cat/cat.service.ts
@@ -1,4 +1,13 @@
+import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Cat, CatDocument } from './schema/cat.schema';
 
 @Injectable()
-export class CatService {}
+export class CatService {
+  constructor(@InjectModel(Cat.name) private catModel: Model<CatDocument>) {}
+
+  async findAll(): Promise<Cat[]> {
+    return this.catModel.find().exec();
+  }
+}
