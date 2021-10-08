c5422a6fc9296c8e9ee854609992846d55691f66
marlloncsoares <marlonmcs@hotmail.com>
2021-10-08 15:07:16 -0300

    feat(config): adiciona GraphQL

---
+++ src/cat/cat.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { CatService } from './cat.service';
+import { CatResolver } from './cat.resolver';
+
+@Module({
+  providers: [CatService, CatResolver]
+})
+export class CatModule {}
